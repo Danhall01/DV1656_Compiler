4ba7df57cea6b68e52be560eafee8d6d544804d2
Daniel <wraithe01@hotmail.com>
2024-01-18 16:19:02 +0100

    fix(): Makefile now compiles properly

--- main.c
+++ main.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 
-extern int count = 0;
-extern int yylex(void);
+extern int   count;
+extern int   yylex(void);
 extern FILE* yyin;
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv)
+{
     // Reads from file if a file name is passed as an argument. Otherwise, reads from stdin.
-	if (argc > 1)
-	{
-		if (!(yyin = fopen(argv[1], "r")))
-		{
-			perror(argv[1]);
-			return 1;
-		}
-	}
-	yylex();
+    if (argc > 1)
+    {
+        if (!(yyin = fopen(argv[1], "r")))
+        {
+            perror(argv[1]);
+            return 1;
+        }
+    }
+    yylex();
     printf("\nNumber of Capital letters "
-	    "in the given input - %d\n", count);
+           "in the given input - %d\n",
+           count);
 
     return 0;
 }
