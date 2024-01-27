e8ecd0e7cc50fd36652ba69e7fa556d8cba4e13a
Daniel <wraithe01@hotmail.com>
2024-01-27 20:41:58 +0100

    feat(): ported Node.h from Cpp to C

--- Node.h
+++ Node.h
@@ -1,57 +1,21 @@
 #pragma once
 
-#include <list>
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <unistd.h>
-#include <sys/wait.h>
+#include <stdint.h>
 
-using namespace std;
+typedef struct Node
+{
+    int32_t id;
+    int32_t lineno;
+    char*   type;
+    char*   value;
 
+    struct Node** children;
+    uint32_t      size;
+    uint32_t      capacity;
+} Node_s;
 
-class Node {
-public:
-	int id, lineno;
-	string type, value;
-	list<Node*> children;
-	Node(string t, string v, int l) : type(t), value(v), lineno(l){}
-	Node()
-	{
-		type = "uninitialised";
-		value = "uninitialised"; }   // Bison needs this.
-  
-	void print_tree(int depth=0) {
-		for(int i=0; i<depth; i++)
-		cout << "  ";
-		cout << type << ":" << value << endl; //<< " @line: "<< lineno << endl;
-		for(auto i=children.begin(); i!=children.end(); i++)
-		(*i)->print_tree(depth+1);
-	}
-  
-	void generate_tree() {
-		std::ofstream outStream;
-		char* filename = "tree.dot";
-	  	outStream.open(filename);
+Node_s* initNodeTree(char* type, char* value, int32_t lineno);
+void    addSubTree(Node_s node[static 1], Node_s newNode[static 1]);
 
-		int count = 0;
-		outStream << "digraph {" << std::endl;
-		generate_tree_content(count, &outStream);
-		outStream << "}" << std::endl;
-		outStream.close();
-
-		printf("\nBuilt a parse-tree at %s. Use 'make tree' to generate the pdf version.\n", filename);
-  	}
-
-  	void generate_tree_content(int &count, ofstream *outStream) {
-	  id = count++;
-	  *outStream << "n" << id << " [label=\"" << type << ":" << value << "\"];" << endl;
-
-	  for (auto i = children.begin(); i != children.end(); i++)
-	  {
-		  (*i)->generate_tree_content(count, outStream);
-		  *outStream << "n" << id << " -> n" << (*i)->id << endl;
-	  }
-  }
-
-};
+void printTree(Node_s node[static 1], int depth);
+void generateTree(Node_s node[static 1]);
