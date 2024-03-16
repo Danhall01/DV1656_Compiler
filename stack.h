ea62e7b8013896f8986f8e4a6c39632ca795548e
Daniel <wraithe01@hotmail.com>
2024-03-16 21:21:21 +0100

    init(Interpreter): Added dynamic stack

---
+++ stack.h
@@ -0,0 +1,43 @@
+#ifndef _STACK_H_
+#define _STACK_H_
+
+#include <inttypes.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct _DynamicStack
+{
+    size_t  size;
+    size_t  capacity;
+    int32_t stack[];
+} stack_s;
+static stack_s* CreateStack()
+{
+    size_t   capacity = 64;
+    stack_s* stack    = (stack_s*) malloc(sizeof(*stack) + sizeof(int32_t[capacity]));
+    if (stack)
+        *stack = (stack_s){ .size = 0, .capacity = capacity };
+    return stack;
+}
+static void DestroyStack(stack_s** stack) { free(*stack); }
+static void Push(stack_s** stack, int32_t value)
+{
+    if (stack == NULL)
+        return;
+
+    if ((*stack)->size + 1 >= (*stack)->capacity)
+    {
+        (*stack) = (stack_s*) realloc((*stack), sizeof(stack_s) + (*stack)->capacity * 1.4);
+        if ((*stack) == NULL)
+            abort();
+    }
+    (*stack)->stack[(*stack)->size++] = value;
+}
+static int32_t Pop(stack_s** stack)
+{
+    if (stack == NULL || (*stack)->size == 0)
+        return -1;
+    return (*stack)->stack[--(*stack)->size];
+}
+
+#endif  // _STACK_H_
